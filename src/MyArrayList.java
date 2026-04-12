ca692500ec207a3bde954caeb7efd796bfc7fb6e
HappyGamerKZ <aylbekov9@gmail.com>
2026-04-12 21:59:45 +0500

    add MyArrayList
    создаем класс для создания массивов и последующей работой с ними на основе интерфейса MyList.
    заодно прописываем методы включенные в MyList.
    Для работы с методом Sort была добавлена проверка элемента на наличие интерфейса Comparable

---
+++ src/MyArrayList.java
@@ -0,0 +1,177 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MyArrayList<T extends Comparable<T>> implements MyList<T> {
+    private Object[] elements;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public MyArrayList() {
+        this.elements = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+    }
+
+    private void resize() {
+        if (size == elements.length) {
+            Object[] newElements = new Object[elements.length * 2];
+            for (int i = 0; i < elements.length; i++) {
+                newElements[i] = elements[i];
+            }
+            elements = newElements;
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+
+    @Override
+    public void add(T item) {
+        resize();
+        elements[size++] = item;
+    }
+
+    @Override
+    public void set(int index, T item) {
+        checkIndex(index);
+        elements[index] = item;
+    }
+
+    @Override
+    public void add(int index, T item) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        resize();
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = item;
+        size++;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        add(0, item);
+    }
+
+    @Override
+    public void addLast(T item) {
+        add(size, item);
+    }
+
+    @Override
+    public T get(int index) {
+        checkIndex(index);
+        return (T) elements[index];
+    }
+
+    @Override
+    public T getFirst() {
+        return get(0);
+    }
+
+    @Override
+    public T getLast() {
+        return get(size - 1);
+    }
+
+    @Override
+    public void remove(int index) {
+        checkIndex(index);
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        elements[--size] = null;
+    }
+
+    @Override
+    public void removeFirst() {
+        remove(0);
+    }
+
+    @Override
+    public void removeLast() {
+        remove(size - 1);
+    }
+
+    @Override
+    public void sort() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                T a = (T) elements[j];
+                T b = (T) elements[j + 1];
+                if (a.compareTo(b) > 0) {
+                    elements[j] = b;
+                    elements[j + 1] = a;
+                }
+            }
+        }
+    }
+    @Override
+    public int indexOf(Object Object) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(Object)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object Object) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (elements[i].equals(Object)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean exists(Object Object) {
+        return indexOf(Object) != -1;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] array = new Object[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = elements[i];
+        }
+        return array;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator(){
+        return new Iterator<T>(){
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) elements[index++];
+            }
+        };
+    }
+}
